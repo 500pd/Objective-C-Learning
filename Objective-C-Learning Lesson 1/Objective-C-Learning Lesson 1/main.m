d44d2ff758e8729f6561b3c27d09423adfb54b0a
Peter Dyhr <paadyhr@gmail.com>
2017-07-01 16:17:47 +0200

    Add Lesson 1: Hello, World!

---
+++ Objective-C-Learning Lesson 1/Objective-C-Learning Lesson 1/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Peter András Adorján Dyhr on 01/07/2017.
+//  Copyright © 2017 Peter András Adorján Dyhr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+		// insert code here...
+		NSLog(@"Hello, World!");
+	}
+	
+	//	Ifølge guiden virker dette. Det er dog uddateret, og Xcodes egen måde ovenfor er den, der virker
+	/*	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	 NSLog(@"Hello, World!");
+	 [pool drain];*/
+
+	return 0;
+}
