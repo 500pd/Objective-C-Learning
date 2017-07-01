181b2860edf9e8713996ce2ec30f8c2449fbfd0d
Peter Dyhr <paadyhr@gmail.com>
2017-07-01 18:12:23 +0200

    Move program 3.2 stuff around so both 3.1 and 3.2 can be run

--- Objective-C-Learning Lesson 3/Objective-C-Learning Lesson 3/main.m
+++ Objective-C-Learning Lesson 3/Objective-C-Learning Lesson 3/main.m
@@ -8,20 +8,71 @@
 
 #import <Foundation/Foundation.h>
 
-/*int main(int argc, const char * argv[]) {
-//	Program 3.1
+//	Program 3.2 Stuff
+//	---- Interface ----
+
+@interface Fraction : NSObject
+{
+	int numerator;
+	int denominator;
+}
+
+- (void)showResults;
+- (void)setNumerator:(int)n;
+- (void)setDenominator:(int)d;
+
+@end
+
+//	---- Implementation ----
+
+@implementation Fraction
+
+- (void)showResults {
+	NSLog(@"This is a fraction with a value of %d/%d", numerator, denominator);
+}
+
+- (void)setNumerator:(int)n {
+	numerator = n;
+}
+
+- (void)setDenominator:(int)d {
+	denominator = d;
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+	//	Program 3.1
 	@autoreleasepool {
 		int numerator = 2;
 		int denominator = 5;
 		NSLog(@"This is a fraction with a value of %d/%d", numerator, denominator);
 	}
-	return 0;
-}*/
+	//	Program 3.2 'main' part
+	@autoreleasepool {
+		//	Create an instance of Fraction
+		Fraction *myFraction;
+		
+		myFraction = [[Fraction alloc] init];
+		
+		//	Set myFraction to 2/5
+		[myFraction setNumerator:2];
+		[myFraction setDenominator:5];
+		
+		//	Display the	value of myFraction
+		[myFraction showResults];
+		
+		//	Clear up memory (we can't use "[myFraction release];" anymore so we use "myFraction = nil;" instead
+		myFraction = nil;
+	}
 
-//	Program 3.2
+	return 0;
+}
+
+//	Program 3.2 (All of it, requires the above to be commented out)
 //	---- Interface ----
 
-@interface Fraction : NSObject
+/*@interface Fraction : NSObject
 {
 	int numerator;
 	int denominator;
@@ -71,4 +122,4 @@ int main(int argc, const char * argv[]) {
 		myFraction = nil;
 	}
 	return 0;
-}
+}*/
