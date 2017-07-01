3f4042de227f6ed34616a417a876b50c8ffa56f5
Peter Dyhr <paadyhr@gmail.com>
2017-07-01 16:21:05 +0200

    Add Lesson 2: Basic Variables

---
+++ Objective-C-Learning Lesson 2/Objective-C-Learning Lesson 2/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  Objective-C-Learning Lesson 2
+//
+//  Created by Peter András Adorján Dyhr on 01/07/2017.
+//  Copyright © 2017 Peter András Adorján Dyhr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+//	Create an integer: i
+		int i;
+//	Set int i to 1
+		i = 1;
+//	Print the text "The variable 'i' has a value of i"
+		NSLog(@"The variable 'i' has a value of %d", i);
+// Change int i to 2
+		i = 2;
+// Print the change
+		NSLog(@"The variable 'i' now has a value of %d", i);
+	}
+// Return 0
+	return 0;
+}
