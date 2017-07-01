890506cc2d1eb9f882c19ba109a699342988394e
Peter Dyhr <paadyhr@gmail.com>
2017-07-01 19:08:54 +0200

    Add Lesson 4: if() statements and Booleans

---
+++ Objective-C-Learning Lesson 4/Objective-C-Learning Lesson 4/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Objective-C-Learning Lesson 4
+//
+//  Created by Peter András Adorján Dyhr on 01/07/2017.
+//  Copyright © 2017 Peter Dyhr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+		BOOL trueOrFalse = YES;
+		if (trueOrFalse)
+			NSLog(@"trueOrFalse is true");
+		if (1)
+			NSLog(@"1 is considered to be true");
+	}
+	return 0;
+}
