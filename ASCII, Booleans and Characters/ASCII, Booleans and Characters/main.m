f3dfb8f8db41b8c78427e6bff23a014112103421
Peter Dyhr <paadyhr@gmail.com>
2017-07-02 13:45:24 +0200

    Add ASCII Values part

--- ASCII, Booleans and Characters/ASCII, Booleans and Characters/main.m
+++ ASCII, Booleans and Characters/ASCII, Booleans and Characters/main.m
@@ -18,7 +18,9 @@ int main(int argc, const char * argv[]) {
 		NSLog(@"This i a char. %c\n And a few more. %c, %c, %c", char1, char2, char3, char4);
 		
 		//	ASCII Values
-		
+		NSLog(@"Hello");	//	Original message
+		NSLog(@"%c%c%c%c%c", ('H' + 1), ('e' + 1), ('l' + 1), ('l' + 1), ('o' + 1));	//	Cypher 1
+		NSLog(@"%c%c%c%c%c", (('H' * 2) % 127), (('e' * 2) % 127), (('l' * 2) % 127), (('l' * 2) % 127), (('o' * 2) % 127));	//	Cypher 2
 	}
 	return 0;
 }
