45a995b3f735b0ba00444d840b5abb7ed58b76db
Peter Dyhr <paadyhr@gmail.com>
2017-07-02 13:06:38 +0200

    Add Floating-Point Operations And fix (hopefully) .gitignore so it ignores stuff that should be ignored

---
+++ Floating-Point Operations/Floating-Point Operations/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Floating-Point Operations
+//
+//  Created by Peter András Adorján Dyhr on 02/07/2017.
+//  Copyright © 2017 Peter Dyhr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
